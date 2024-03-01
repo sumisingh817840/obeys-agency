37bd2d5dcc20e0b41286506c82dc21ba9510ce68
sumit <117508873+Sumit1050002526@users.noreply.github.com>
2024-03-02 00:48:25 +0530

    page1 is done

--- Ques.js
+++ Ques.js
@@ -1,9 +1,7 @@
 
-
-
-
-
-
+// loader animation and loader text animation
+function laoderanimation(){
+    
 //  loader text gsap anim 
 
 var tl = gsap.timeline();
@@ -36,6 +34,8 @@ tl.from(".line h2", {
 
 
     }
+
+
 })
 
 tl.from("#line1-part1", {
@@ -46,19 +46,61 @@ tl.from("#line1-part1", {
 // loader remove anim
 tl.to("#loader", {
     opacity: 0,
-    duration: 0.5,
-    delay: 2
-})
+    // duration: 0.5,
+    duration:0.2
+    // delay: 2
+});
 
 tl.from("#page1",{
     y:1200,
     opacity:0,
     duration:0.7
-})
+});
 
 tl.to("#loader",{
     display:"none"
+});
+
+// hero text anim
+tl.from("#hero h1,h2",{
+    y: 100,
+    stagger: 0.3
+});
+
+ 
+ 
+
+tl.from("#nav",{
+    opacity:0
 })
+
+}
+
+laoderanimation();
+
+// making cursor animation
+function cursoranimation(){
+    document.addEventListener("mousemove",function(dets){
+        gsap.to("#cursor",{
+         left:dets.x,
+         top:dets.y
+        })
+   })
+} 
+cursoranimation();
+
+
+// making magnet effect
+Shery.makeMagnet("#nav-part2 h4" , {
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+  });
+
+
+
+  
+
+
 
 
 
